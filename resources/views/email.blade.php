d4c505e794eb354be42ff840140f99a6f828d0f9
YISECA <yiseca@gmail.com>
2017-06-22 09:12:15 -0500

    inicio del formulario

---
+++ resources/views/email.blade.php
@@ -0,0 +1,15 @@
+<html>
+
+
+
+<h1>Caminata PARQUE ECOLÓGICO DISTRITAL DE MONTAÑA ENTRENUBES DOMINGO 18 de Junio 2017</h1>
+
+<h2>Punto de encuentro: Cra 5 Nº 48p sur polideportivo de Molinos II</h2>
+
+<h2>Hora de salida: 8:00am</h2>
+
+<h1>Su registro fue exitoso!</h1>
+
+
+
+<p></p>
